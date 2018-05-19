1192b74109e9c07f07217ce5417e5690111d364e
qfdk <qfdk2010@gmail.com>
2018-05-19 15:40:51 +0200

    [Dev] 自动填充地址

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -37,25 +37,7 @@
 	
 	<header id="gtco-header" class="gtco-cover gtco-cover-sm" role="banner" style="background-image: url(images/img_bg_1.jpg)" data-stellar-background-ratio="0.5">
 		<div class="overlay"></div>
-		<!--
-		<div class="gtco-container">
-			<div class="row">
-				<div class="col-md-12 col-md-offset-0 text-center">
-					
 
-					<div class="row row-mt-15em">
-						<div class="col-md-12 mt-text animate-box" data-animate-effect="fadeInUp">
-							<span class="intro-text-small">Hand-crafted by <a href="http://gettemplates.co" target="_blank">GetTemplates.co</a></span>
-							<h1 class="cursive-font">Taste all our menu!</h1>	
-						</div>
-						
-					</div>
-							
-					
-				</div>
-			</div>
-		</div>
-		-->
 	</header>
 
 	
@@ -108,14 +90,14 @@
 				</div>
 				<div class="col-md-3">
 					<div class="form-group">
-						<input type="text" class="form-control" id="address" placeholder="Your Address">
+						<input type="text" class="form-control typeahead" id="address" autocomplete="off" data-provide="typeahead">
 					</div>
 				</div>
 				<div class="col-md-3" style="float:right;padding-top:3%;">
-					<button type="submit" class="btn btn-default btn-block" id="checkout">Checkout</button>
+					<button type="submit" class="btn btn-default btn-block" id="checkout">结账</button>
 				</div>
 				<div class="col-md-3" style="float:right;padding-top:3%;">
-					<button type="submit" class="btn btn-default btn-block" id="edit">Edit</button>
+					<button type="submit" class="btn btn-default btn-block" id="edit">保存</button>
 				</div>
 				<div class="col-md-3" style="float:right;padding-top:3%;">
 					<button type="submit" class="btn btn-default btn-block" id="clear">Clear</button>
@@ -123,48 +105,11 @@
 			</div>
 		</div>
 	</div>
-	<!--
-	<div class="gtco-cover gtco-cover-sm" style="background-image: url(images/img_bg_1.jpg)"  data-stellar-background-ratio="0.5">
-		<div class="overlay"></div>
-		<div class="gtco-container text-center">
-			<div class="display-t">
-				<div class="display-tc">
-					<h1>&ldquo; Their high quality of service makes me back over and over again!&rdquo;</h1>
-					<p>&mdash; John Doe, CEO of XYZ Co.</p>
-				</div>	
-			</div>
-		</div>
-	</div>
 
-	<div id="gtco-subscribe">
-		<div class="gtco-container">
-			<div class="row animate-box">
-				<div class="col-md-8 col-md-offset-2 text-center gtco-heading">
-					<h2 class="cursive-font">Subscribe</h2>
-					<p>Be the first to know about the new templates.</p>
-				</div>
-			</div>
-			<div class="row animate-box">
-				<div class="col-md-8 col-md-offset-2">
-					<form class="form-inline">
-						<div class="col-md-6 col-sm-6">
-							<div class="form-group">
-								<label for="email" class="sr-only">Email</label>
-								<input type="email" class="form-control" id="email" placeholder="Your Email">
-							</div>
-						</div>
-						<div class="col-md-6 col-sm-6">
-							<button type="submit" class="btn btn-default btn-block">Subscribe</button>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
-	-->
 	@include('includes.footer')
 
 	</body>
+	<script src="http://docker.qfdk.me/static/js/bootstrap3-typeahead.min.js"></script>
 	<script type="text/javascript">
 	    $(function() {
 			// 列表
@@ -275,6 +220,16 @@
 					+`x</td></tr>`;
 			return tmp;
 		}
+		// 地址自动填充
+		$('#address').typeahead({
+            limit: 10,
+            source: function (query, process) {
+                return $.get("http://demo.foodxl.fr:2333/?q=" + $('#address').val(), function (data) {
+                    return process(data.label);
+                });
+            }
+		});
+		
 	</script>
 </html>
 
