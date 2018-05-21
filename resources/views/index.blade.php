4fd1eeee80d2c851e5978c25d36b214fe9f503da
qfdk <qfdk2010@gmail.com>
2018-05-21 21:13:21 +0200

    [Dev] 测试遮罩动画

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -100,120 +100,23 @@
 	</div>
 </div>
 
-<!-- <div id="gtco-features">
-	<div class="gtco-container">
-		<div class="row">
-			<div class="col-md-8 col-md-offset-2 text-center gtco-heading animate-box">
-				<h2 class="cursive-font">Our Services</h2>
-				<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-md-4 col-sm-6">
-				<div class="feature-center animate-box" data-animate-effect="fadeIn">
-					<span class="icon">
-						<i class="ti-face-smile"></i>
-					</span>
-					<h3>Happy People</h3>
-					<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
-				</div>
-			</div>
-			<div class="col-md-4 col-sm-6">
-				<div class="feature-center animate-box" data-animate-effect="fadeIn">
-					<span class="icon">
-						<i class="ti-thought"></i>
-					</span>
-					<h3>Creative Culinary</h3>
-					<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
-				</div>
-			</div>
-			<div class="col-md-4 col-sm-6">
-				<div class="feature-center animate-box" data-animate-effect="fadeIn">
-					<span class="icon">
-						<i class="ti-truck"></i>
-					</span>
-					<h3>Food Delivery</h3>
-					<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
-				</div>
-			</div>
-		</div>
-	</div>
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">菜品详情</h4>
+      </div>
+      <div class="modal-body" id="myContent">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button id="commande" type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
 </div>
-
-<div class="gtco-cover gtco-cover-sm" style="background-image: url(images/img_bg_1.jpg)"  data-stellar-background-ratio="0.5">
-	<div class="overlay"></div>
-	<div class="gtco-container text-center">
-		<div class="display-t">
-			<div class="display-tc">
-				<h1>&ldquo; Their high quality of service makes me back over and over again!&rdquo;</h1>
-				<p>&mdash; John Doe, CEO of XYZ Co.</p>
-			</div>	
-		</div>
-	</div>
-</div> -->
-
-<!-- <div id="gtco-counter" class="gtco-section">
-	<div class="gtco-container">
-		<div class="row">0
-			<div class="col-md-8 col-md-offset-2 text-center gtco-heading animate-box">
-				<h2 class="cursive-font primary-color">Fun Facts</h2>
-				<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-md-3 col-sm-6 animate-box" data-animate-effect="fadeInUp">
-				<div class="feature-center">
-					<span class="counter js-counter" data-from="0" data-to="5" data-speed="5000" data-refresh-interval="50">1</span>
-					<span class="counter-label">Avg. Rating</span>
-				</div>
-			</div>
-			<div class="col-md-3 col-sm-6 animate-box" data-animate-effect="fadeInUp">
-				<div class="feature-center">
-					<span class="counter js-counter" data-from="0" data-to="43" data-speed="5000" data-refresh-interval="50">1</span>
-					<span class="counter-label">Food Types</span>
-				</div>
-			</div>
-			<div class="col-md-3 col-sm-6 animate-box" data-animate-effect="fadeInUp">
-				<div class="feature-center">
-					<span class="counter js-counter" data-from="0" data-to="32" data-speed="5000" data-refresh-interval="50">1</span>
-					<span class="counter-label">Chef Cook</span>
-				</div>
-			</div>
-			<div class="col-md-3 col-sm-6 animate-box" data-animate-effect="fadeInUp">
-				<div class="feature-center">
-					<span class="counter js-counter" data-from="0" data-to="1985" data-speed="5000" data-refresh-interval="50">1</span>
-					<span class="counter-label">Year Started</span>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-
-<div id="gtco-subscribe">
-	<div class="gtco-container">
-		<div class="row animate-box">
-			<div class="col-md-8 col-md-offset-2 text-center gtco-heading">
-				<h2 class="cursive-font">Subscribe</h2>
-				<p>Be the first to know about the new templates.</p>
-			</div>
-		</div>
-		<div class="row animate-box">
-			<div class="col-md-8 col-md-offset-2">
-				<form class="form-inline">
-					<div class="col-md-6 col-sm-6">
-						<div class="form-group">
-							<label for="email" class="sr-only">Email</label>
-							<input type="email" class="form-control" id="email" placeholder="Your Email">
-						</div>
-					</div>
-					<div class="col-md-6 col-sm-6">
-						<button type="submit" class="btn btn-default btn-block">Subscribe</button>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
-</div> -->
 
 @include('includes.footer')
 
@@ -239,15 +142,28 @@
 		});
 	});
 
-	function add(goods_id){
-		$.get(`api/cart/add?goods_id=`+goods_id+`&number=1`,function(){
-			alert('添加成功');
-		});
+	function modal(obj){
+		$('#myContent').html(`<div class="col-lg-4 col-md-4 col-sm-6">
+					<figure>
+						<div class="overlay"><i class="ti-plus"></i></div>
+						<img src="`+obj.goods_img+`" alt="Image" class="img-responsive">
+					</figure>
+					<div class="fh5co-text">
+						<h2>`+obj.goods_name+`</h2>
+						<p>`+obj.goods_desc+`</p>
+						<p><span class="price cursive-font">€`+obj.shop_price+`</span></p>
+					</div>
+			</div>`);
+			$('#commande').on('click',function(){
+				$.get(`api/cart/add?goods_id=`+obj.goods_id+`&number=1`,function(){
+					alert('添加成功');
+				});
+			});
 	}
-	
+
 	function getGoods(obj){
 		var tmp = `<div class="col-lg-4 col-md-4 col-sm-6">
-				<a href="#" class="fh5co-card-item image-popup" onclick="add(`+obj.goods_id+`);return false;">
+				<a href="#" class="fh5co-card-item image-popup" data-toggle="modal" data-target="#myModal" onclick="modal(`+obj+`);return false;">
 					<figure>
 						<div class="overlay"><i class="ti-plus"></i></div>
 						<img src="`+obj.goods_img+`" alt="Image" class="img-responsive">
