766e8865d95e462f2810339227f114aa5fb22f8f
qfdk <qfdk2010@gmail.com>
2018-10-02 21:30:56 +0200

    [Fix] css

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -9,8 +9,8 @@
 	<meta name="keywords" content="FOODXL" />
 	<meta name="author" content="FOODXL" />
 
-	// <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">
-	// <link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet">
 	
 	<!-- Animate.css -->
 	<link rel="stylesheet" href="css/animate.css">
